f8b0ba65d1802db99ded8b105edbd98d0345ff8b
Rohit <rohitp934@gmail.com>
2023-06-25 14:58:24 +0530

    Fully converted AppHeader to tailwind

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,14 +1,23 @@
 <script lang="ts" setup>
-const alpine = useAppConfig().alpine
+const alpine = useAppConfig().alpine;
 
-const show = ref(false)
+const show = ref(false);
 </script>
 
 <template>
-  <header :class="alpine.header?.position || 'left'" class="relative grid grid-cols-12 items-center gap-4 py-16">
-    <div class="menu absolute flex hover:text-purple-500 sm:hidden">
-      <button aria-label="Navigation Menu" @click="(show = !show)">
-        <svg width="24" height="24" viewBox="0 0 68 68" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <header
+    :class="alpine.header?.position || 'left'"
+    class="relative grid grid-cols-12 items-center gap-4 py-16"
+  >
+    <div class="menu absolute right-0 flex hover:text-purple-500 sm:hidden">
+      <button aria-label="Navigation Menu" @click="show = !show">
+        <svg
+          width="24"
+          height="24"
+          viewBox="0 0 68 68"
+          fill="currentColor"
+          xmlns="http://www.w3.org/2000/svg"
+        >
           <path
             d="M8 34C8 32.1362 8 31.2044 8.30448 30.4693C8.71046 29.4892 9.48915 28.7105 10.4693 28.3045C11.2044 28 12.1362 28 14 28C15.8638 28 16.7956 28 17.5307 28.3045C18.5108 28.7105 19.2895 29.4892 19.6955 30.4693C20 31.2044 20 32.1362 20 34C20 35.8638 20 36.7956 19.6955 37.5307C19.2895 38.5108 18.5108 39.2895 17.5307 39.6955C16.7956 40 15.8638 40 14 40C12.1362 40 11.2044 40 10.4693 39.6955C9.48915 39.2895 8.71046 38.5108 8.30448 37.5307C8 36.7956 8 35.8638 8 34Z"
           />
@@ -23,21 +32,23 @@ const show = ref(false)
     </div>
 
     <div
-      class="overlay sm:hidden absolute top-24 bg-gray-100 dark:bg-gray-900 border-2 border-solid border-gray-200 dark:border-gray-800 rounded-md p-6 text-lg leading-8 font-medium transition-all backdrop-blur-md will-change-auto"
+      class="overlay absolute right-0 top-28 rounded-md border-2 border-solid border-gray-200 bg-gray-100 p-6 text-right text-lg font-medium leading-8 backdrop-blur-md transition-all will-change-auto dark:border-gray-800 dark:bg-gray-900 sm:hidden"
       :class="[show && 'show']"
     >
       <MainNav @link-click="show = !show" />
     </div>
 
-    <div class="logo text-3xl h-12 font-extrabold flex col-span-12 sm:col-span-4 sm:col-start-auto ">
+    <div
+      class="logo col-span-12 flex h-12 text-3xl font-extrabold sm:col-span-4 sm:col-start-auto"
+    >
       <NuxtLink v-if="alpine.header?.logo" to="/">
-        <h1 class="hover:text-purple-200">
-          Rohit Prakash
-        </h1>
+        <h1 class="hover:text-purple-200">Rohit Prakash</h1>
       </NuxtLink>
     </div>
 
-    <div class="main-nav hidden sm:flex sm:col-span-8 sm:text-xl sm:leading-6 sm:font-medium">
+    <div
+      class="main-nav hidden sm:col-span-8 sm:flex sm:justify-end sm:text-xl sm:font-medium sm:leading-6"
+    >
       <MainNav />
     </div>
   </header>
@@ -46,46 +57,12 @@ const show = ref(false)
 <style scoped lang="ts">
 css({
   header: {
-    '--header-padding': '{space.16}',
-    '--logo-height': '{space.8}',
-    '.menu': {
-      '.left &&': {
-        right: 0
-      }
-    },
     '.overlay': {
       '&:not(.show)': {
         opacity: 0,
         transform: 'translateY(-10px) rotateY(-8deg) rotateX(-20deg)',
-        '.left &&': {
-          transform: 'translateY(-10px) rotateY(8deg) rotateX(-20deg)',
-        },
         pointerEvents: 'none'
       },
-      '.left &&': {
-        right: 0,
-        padding: '{space.6}',
-        paddingLeft: '{space.16}',
-        textAlign: 'right'
-      }
-    },
-    '.main-nav': {
-      '@sm': {
-        // gridColumn: 'span 8 / span 8',
-        // fontSize: '{text.xl.fontSize}',
-        // lineHeight: '{text.xl.lineHeight}',
-        // fontWeight: '{fontWeight.medium}',
-        '.center &&': {
-          gridColumn: 'span 12 / span 12',
-          justifyContent: 'center',
-        },
-        '.right &&': {
-          justifyContent: 'flex-start',
-        },
-        '.left &&': {
-          justifyContent: 'flex-end',
-        },
-      }
     },
   }
 })
